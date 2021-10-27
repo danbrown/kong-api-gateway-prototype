439f961fb981cb4208d23d834bc7bcdc7a53e7be
danbrown <danielbrown25.9.2@gmail.com>
2021-10-27 00:09:50 -0300

    initial

---
+++ mock-omega/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+const HOST = process.env.HOST || "0.0.0.0";
+
+app.get("/", (req, res) => {
+  res.send("Omega!");
+});
+
+app.listen(PORT, HOST);
